e9b931b9e783df0ae1f989d20fc7bcf170083490
Samara <samacard15@hotmail.com>
2022-04-21 16:29:17 -0300

    exercício 3

--- fundamentos/bloco-4-intruducao-a-javascrit-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-intruducao-a-javascrit-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,18 +1,33 @@
-// Exercício 1:Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+// // Exercício 1:Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// };
+
+// // Exercício 2: Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     resultado += numbers[index];
+// }
+
+// console.log(resultado);
+
+//Exercício 3: Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+// A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
-};
+let soma = 0;
 
-// Exercício 2: Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
-
-for (let index = 0; index < numbers.length; index += 1) {
-    resultado += numbers[index];
+for (let index = 0; index < numbers.length; index += 1){
+    soma += numbers[index];
 }
 
-console.log(resultado);
+let media = soma / numbers.length;
+
+console.log(media);
