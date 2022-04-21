aaa437f0ac5fa685ab45086d436b64315df56434
Samara <samacard15@hotmail.com>
2022-04-21 12:49:20 -0300

    exercício 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -43,3 +43,22 @@ if (number > 0) {
     console.log('zero');
 }
 
+// Exercício 5: Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+const anguloA = - 220;
+const anguloB = - 220;
+const anguloC = 220;
+
+let somaDeTodosOsAngulos = anguloA + anguloB + anguloC;
+
+let todosOsAngulosForemPosivitos = anguloA > 0 && anguloB > 0 && anguloC > 0;
+
+if (todosOsAngulosForemPosivitos){
+    if (somaDeTodosOsAngulos === 180) {
+        console.log(true);
+    } else {
+        console.log(false);
+    };
+} else {
+    console.log('Erro: ângulo inválido');
+};
