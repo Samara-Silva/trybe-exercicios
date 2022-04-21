0142ae47a091b703cea0b99c6b2fee07e3b60291
Samara <samacard15@hotmail.com>
2022-04-21 12:42:32 -0300

    Exercício 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,4 +1,4 @@
-// Exercício 1
+// Exercício 1: Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
 
 const a = 10;
 const b = 5;
@@ -9,7 +9,7 @@ console.log('Multipĺicação: ' + (a * b));
 console.log('Divisão: ' + (a / b));
 console.log('Módulo: ' + (a % b));
 
-// Exercício 2
+// Exercício 2 : Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 
 if (a > b) {
     console.log("'a' é maior que 'b'");
@@ -17,7 +17,7 @@ if (a > b) {
     console.log("'b' é maior que 'a'");
 };
 
-// Exercício 3
+// Exercício 3 : Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
 
 const a = 6;
 const b = 4;
@@ -30,3 +30,16 @@ if (a > b && a > c) {
 } else {
     console.log('O maior número é: ' + c + ' (c)');
 };
+
+// Exercício 4: Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+const number = 10;
+
+if (number > 0) {
+    console.log('positive');
+} else if (number < 0) {
+    console.log('negative');
+} else {
+    console.log('zero');
+}
+
