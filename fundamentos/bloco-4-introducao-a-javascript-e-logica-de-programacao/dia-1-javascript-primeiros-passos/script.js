bf1bf2d026b9a3eb9ffbd02400c1f714c9a8721e
Samara <samacard15@hotmail.com>
2022-04-21 12:31:28 -0300

    Exercício 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,7 +1,19 @@
+// Exercício 1
+
 const a = 10;
 const b = 5;
 
 console.log ('Soma: ' + (a + b));
 console.log ('Subtração: ' + (a - b));
 console.log('Multipĺicação: ' + (a * b));
-comsole.log(Divisão)
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b));
+
+// Exercício 2
+
+if (a > b) {
+    console.log("'a' é maior que 'b'");
+} else {
+    console.log("'b' é maior que 'a'");
+}
+
