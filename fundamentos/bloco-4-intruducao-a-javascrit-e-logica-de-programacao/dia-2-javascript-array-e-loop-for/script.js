ad7de9b8e614caadd34bfdd8938dd5b84a8bdf96
Samara <samacard15@hotmail.com>
2022-04-21 16:43:36 -0300

    Exercício 5

--- fundamentos/bloco-4-intruducao-a-javascrit-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-intruducao-a-javascrit-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -34,16 +34,29 @@
 
 // Exercício 4: Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+
+// for (let index = 0; index < numbers.length; index += 1){
+//     resultado += numbers[index];
+// }
+// resultado = resultado / numbers.length;
+
+// if (resultado > 20) {
+//     console.log('valor maior que 20');
+// } else {
+//     console.log('valor menor ou igual a 20');
+// }
+
+//Exercicio 5: Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
+ let maiorNumero = numbers[0];
 
-for (let index = 0; index < numbers.length; index += 1){
-    resultado += numbers[index];
+ for (let index = 0; index < numbers.length; index += 1) {
+if (numbers[index] > maiorNumero) {
+    maiorNumero = numbers[index];
 }
-resultado = resultado / numbers.length;
+ };
 
-if (resultado > 20) {
-    console.log('valor maior que 20');
-} else {
-    console.log('valor menor ou igual a 20');
-}
+ console.log(maiorNumero);
