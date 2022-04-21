3f1cf014b0aa03a8844c8299edf00b301b6b263f
Samara <samacard15@hotmail.com>
2022-04-21 12:37:59 -0300

    EXERCICIO 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -15,5 +15,18 @@ if (a > b) {
     console.log("'a' é maior que 'b'");
 } else {
     console.log("'b' é maior que 'a'");
-}
+};
 
+// Exercício 3
+
+const a = 6;
+const b = 4;
+const c = 2;
+
+if (a > b && a > c) {
+    console.log('O maior número é: ' + a + ' (a)'); 
+} else if (b > a && b > c) {
+    console.log ('o maior número é ' + b + ' (b)');
+} else {
+    console.log('O maior número é: ' + c + ' (c)');
+};
