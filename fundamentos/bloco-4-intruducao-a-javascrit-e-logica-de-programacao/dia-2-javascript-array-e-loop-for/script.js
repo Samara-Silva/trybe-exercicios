a1cac54b911ec0df5fab8c2ce21161c5084a8e58
Samara <samacard15@hotmail.com>
2022-04-21 17:04:20 -0300

    Exercicio 7

--- fundamentos/bloco-4-intruducao-a-javascrit-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-intruducao-a-javascrit-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -63,18 +63,31 @@
 
 //Exercício 6: Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] % 2 !== 0) {
+//         resultado += 1;
+//     }
+// }
+
+// if (resultado === 0) {
+//     console.log('nenhum valor impar encontrado');
+// } else {
+//     console.log(resultado);
+// }
+
+//Exercício 7: Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
+let menorNumero = numbers[0];
 
-
-for (let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] % 2 !== 0) {
-        resultado += 1;
+for (let index = 1; index < numbers.length; index += 1){
+    if (numbers[index] < menorNumero) {
+        menorNumero = numbers[index];
     }
 }
 
-if (resultado === 0) {
-    console.log('nenhum valor impar encontrado');
-} else {
-    console.log(resultado);
-}
+console.log(menorNumero);
