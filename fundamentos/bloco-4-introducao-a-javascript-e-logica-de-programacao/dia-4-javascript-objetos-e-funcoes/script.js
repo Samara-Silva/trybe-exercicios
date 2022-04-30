47f8ba5f12c71dfa4ecd65413423493a3325b4e1
Samara <samacard15@hotmail.com>
2022-04-30 02:56:51 -0300

    Exerício 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,10 +1,22 @@
 // 1
 
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+
+// console.log('Bem vinda,' + ' ' + info.personagem);
+
+
+
+
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+    recorrente: 'Sim'
+};
 
-
-console.log('Bem vinda,' + ' ' + info.personagem);
+console.log(info);
