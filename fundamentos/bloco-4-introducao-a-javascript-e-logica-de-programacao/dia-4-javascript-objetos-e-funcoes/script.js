fd4c67af33caaf3c0729e6fb72b4fabbf3eb7e14
Samara <samacard15@hotmail.com>
2022-04-30 03:13:59 -0300

    Exericio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,13 +1,13 @@
-1
+// 1
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
 
-console.log('Bem vinda,' + ' ' + info.personagem);
+// console.log('Bem vinda,' + ' ' + info.personagem);
 
 
 //2
@@ -19,13 +19,18 @@ let info = {
     recorrente: 'Sim'
 };
 
-console.log(info);
-
-for (info in info) {
-    console.log(info);
+let info2 = {
+    personagem: 'e Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'e O último MacPatinhas',
+    recorrente: 'Ambos recorrentes',
 }
 
 for (index in info) {
     console.log(info[index]);
 };
+
+for (index in info2) {
+    console.log(info2[index]);
+}
 
