57970c15057f08e1e1d9860134ce07424d1d5322
Samara <samacard15@hotmail.com>
2022-04-30 02:23:11 -0300

    Exerício3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -13,14 +13,40 @@
 
 // console.log(fatorialDe10);
 
-// Exercicio 2: 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+// Exercicio 2: Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
-let word = 'tryber';
+// let word = 'tryber';
+// 
+// let palavraInvertida = '';
+// 
+// for (let index = 0; index < word.length; index += 1) {
+    // palavraInvertida += word[word.length - 1 - index];
+// }
+// 
+// console.log(palavraInvertida);
 
-let palavraInvertida = '';
+// Exercício 3: Considere o array de strings abaixo:
+// Copiar
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
-for (let index = 0; index < word.length; index += 1) {
-    palavraInvertida += word[word.length - 1 - index];
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maiorPalavra = array [0];
+let menorPalavra = array [0];
+
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length > maiorPalavra.length) {
+        maiorPalavra = array[index];
+    }
 }
 
-console.log(palavraInvertida);
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length < menorPalavra.length) {
+        menorPalavra = array[index];
+    }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);
