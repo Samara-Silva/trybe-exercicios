aac28dd14ed025bfe81e42597885e9ecd2794e96
Samara <samacard15@hotmail.com>
2022-05-01 23:12:05 -0300

    Adc exerc 6 parte II função

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -134,24 +134,38 @@
 // Array de teste: [2, 3, 2, 5, 8, 2, 3]; .
 // Valor esperado no retorno da função: 2 .
 
-function maisRepetido(numeros) {
-  let contRepetido = 0;
-  let contNumero = 0;
-  let indexNumeroRepetido = 0;
-  for (let index in numeros) {
-    let verificaNumero = numeros[index];
-    for (let index2 in numeros) {
-      if(verificaNumero === numeros[index2]) {
-        contNumero += 1;
-      }
-    }
-    if (contNumero > contRepetido) {
-      contRepetido = contNumero;
-      indexNumeroRepetido = index;
-    }
-    contNumero = 0;
-  }
-  return numeros[indexNumeroRepetido];
-};
+// function maisRepetido(numeros) {
+//   let contRepetido = 0;
+//   let contNumero = 0;
+//   let indexNumeroRepetido = 0;
+//   for (let index in numeros) {
+//     let verificaNumero = numeros[index];
+//     for (let index2 in numeros) {
+//       if(verificaNumero === numeros[index2]) {
+//         contNumero += 1;
+//       }
+//     }
+//     if (contNumero > contRepetido) {
+//       contRepetido = contNumero;
+//       indexNumeroRepetido = index;
+//     }
+//     contNumero = 0;
+//   }
+//   return numeros[indexNumeroRepetido];
+// };
 
-console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3]));
+// console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3]));
+
+// Exercício 6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+// Valor de teste: N = 5 .
+// Valor esperado no retorno da função: 1+2+3+4+5 = 15 .
+
+function somaTodasNumeros(numeros) {
+  let total = 0;
+  for (let index = 1; index <= numeros; index += 1) {
+    total = total + index;
+  }
+  return total;
+}
+
+console.log(somaTodasNumeros(5));
