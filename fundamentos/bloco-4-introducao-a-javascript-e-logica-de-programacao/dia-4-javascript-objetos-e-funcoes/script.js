a75678ed6dc491a525253b04c80e7ce4be907c49
Samara <samacard15@hotmail.com>
2022-05-01 23:09:32 -0300

    adiciona exerc 5 parte II função

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -117,14 +117,41 @@
 // Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
 // Valor esperado no retorno da função: Fernanda .
 
-function maiorPalavra(palavras) {
-  let maiorPalavra = palavras[0];
-  for (let indice in palavras) {
-    if (maiorPalavra.length < palavras[indice].length) {
-      maiorPalavra = palavras[indice];
+// function maiorPalavra(palavras) {
+//   let maiorPalavra = palavras[0];
+//   for (let indice in palavras) {
+//     if (maiorPalavra.length < palavras[indice].length) {
+//       maiorPalavra = palavras[indice];
+//     }
+//   }
+//   return maiorPalavra;
+// };
+
+// console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+
+// Exercício 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+// Array de teste: [2, 3, 2, 5, 8, 2, 3]; .
+// Valor esperado no retorno da função: 2 .
+
+function maisRepetido(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+  for (let index in numeros) {
+    let verificaNumero = numeros[index];
+    for (let index2 in numeros) {
+      if(verificaNumero === numeros[index2]) {
+        contNumero += 1;
+      }
     }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
   }
-  return maiorPalavra;
+  return numeros[indexNumeroRepetido];
 };
 
-console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3]));
