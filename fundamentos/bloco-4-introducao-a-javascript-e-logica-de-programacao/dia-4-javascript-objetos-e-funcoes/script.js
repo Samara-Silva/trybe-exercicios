0c66432f7bed7c2af20c46c9167d3ad7d2e5648d
Samara <samacard15@hotmail.com>
2022-05-01 22:53:45 -0300

    exercícios 2 parte II função

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -69,14 +69,30 @@
 // Retorno esperado: false
 
 
-function verificaPalindrome(string) {
-  let reverse = string.split('').reverse().join('');
-  if (reverse === string) {
-    return true;
-  } else {
-    return false;
-  }
-};
+// function verificaPalindrome(string) {
+//   let reverse = string.split('').reverse().join('');
+//   if (reverse === string) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 
-console.log(verificaPalindrome('arara'));
-console.log(verificaPalindrome('desenvolvimento'));
+// console.log(verificaPalindrome('arara'));
+// console.log(verificaPalindrome('desenvolvimento'));
+
+// Exercício 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// Array de teste: [2, 3, 6, 7, 10, 1]; .
+// Valor esperado no retorno da função: 4 .
+
+function indiceDoMaior(numeros) {
+  let indiceMaior = 0;
+  for (let indice in numeros) {
+    if (numeros[indiceMaior] < numeros[indice]) {
+      indiceMaior = indice;
+    }
+  }
+  return indiceMaior;
+}
+
+console.log(indiceDoMaior([2, 3, 6, 7, 10, 1]))
